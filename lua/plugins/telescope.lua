a8777f9a09d06042d7548e0ae4adbe367954d956
SG Kang <lived2@gmail.com>
2026-08-13 23:56:53 +0900

    telescope: add custom keymap esc for window close

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+      opts.defaults = opts.defaults or {}
+      opts.defaults.mappings = opts.defaults.mappings or {}
+      opts.defaults.mappings.i = opts.defaults.mappings.i or {}
+
+      local actions = require "telescope.actions"
+      opts.defaults.mappings.i["<esc>"] = actions.close
+    end,
+  },
+}
